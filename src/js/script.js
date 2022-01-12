e3c85e7778fbbb612def608b14702d1170fa663c
Владислав Закиев <zakiev.vladislav@stylemix.net>
2022-01-12 22:20:53 +0500

    carousel styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $('.classes_carousel_items').slick();
+    $('.classes_carousel_items').slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 });
